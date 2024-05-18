7cbcbd8fd17b17a7ce073bb74b05b1081f432c67
Ellie Schaefer <eschaefer7d5@gmail.com>
2024-05-18 15:11:34 -0600

    Added more Scripts folder and Flatpak for updates, as well as some basic error handling.

--- nightly.sh
+++ nightly.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # A script to run nightly, preforms system maintinence before shutting down.
 # Ellie Schaefer - 4/30/2024
+echo "Are you ready to shut down?"
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 ~/Scripts/update.sh
-sudo shutdown now
+if [ $? -eq 0 ]
+then
+	echo "Update successful, shutting down now."
+	echo "sudo shutdown now"
+else
+	echo "Update failed, not shutting down."
+fi
--- update.sh
+++ update.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 # A little update script - Ellie Schaefer - 4/30/2024
 echo "Updating Dotfiles..."
-cd ~/.dotfiles
+cd ~/Dotfiles
 git pull
-echo "Updating Packages"
+if [ $? -ne 0 ]
+then
+	echo "Dotfile update failed, stopping now." >&2
+	exit 1
+fi
+echo "Updating Scripts..."
+cd ~/Scripts
+git pull
+if [ $? -ne 0 ]
+then
+	echo "Scripts update failed, stopping now." >&2
+	exit 1
+fi
+echo "Updating FlatPak Packages"
+sudo flatpak update
+if [ $? -ne 0 ]
+then
+	echo "Flatpak update failed, stopping now." >&2
+	exit 1
+fi
+echo "Updating DNF Packages"
 sudo dnf update
+if [ $? -ne 0 ]
+then
+	echo "DNF update failed, stopping now." >&2
+	exit 1
+fi
 echo "all done!"
+exit 0
